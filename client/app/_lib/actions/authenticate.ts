7d5f5c4f7cf32e0ae629d81a7c5662251f48e891
jesus doza <jesus.doza@gmail.com>
2024-06-13 09:01:27 -0500

    add authenticate action

---
+++ client/app/_lib/actions/authenticate.ts
@@ -0,0 +1,22 @@
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      //TODO what happens if error redirect to login again? or display page with error?
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
+}
