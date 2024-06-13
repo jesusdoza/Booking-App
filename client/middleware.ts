92e80104c44003ab6c927ae2b28a2d02c67c5f91
jesus doza <jesus.doza@gmail.com>
2024-06-13 09:15:19 -0500

    moved middleware

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import { authConfig } from "./auth.config";
+import { authConfig } from "./auth-config/auth.config";
 
 //exporting auth property after configuring
 export default NextAuth(authConfig).auth;
