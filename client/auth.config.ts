b0e399b134086eb65fa59c23f56501ccbbfe90c2
jesusdoza <jesus.doza@gmail.com>
2024-06-13 08:09:09 -0500

    add scaffold auth.config.ts

--- client/auth.config.ts
+++ client/auth.config.ts
@@ -1,7 +1,18 @@
 import type { NextAuthConfig } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authConfig = {
   pages: {
     signIn: "/login",
   },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+
+      //TODO check logged status with firbase auth aka check the authorization header
+
+      return true;
+    },
+  },
+  providers: [CredentialsProvider],
 } satisfies NextAuthConfig;
